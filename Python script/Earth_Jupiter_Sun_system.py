f3f29185087d42cdcd10063d0a73e8fdbb256d0b
M <11522779+Shoot@users.noreply.github.com>
2024-02-24 14:44:20 +0300

    undone

--- Python script/Earth_Jupiter_Sun_system.py
+++ Python script/Earth_Jupiter_Sun_system.py
@@ -10,7 +10,7 @@ def init():
     line2.set_data([], [])
     title_text.set_text('')
     return line1,line2,title_text
-	
+
 
 def force_es(r_):
     F = np.zeros(2)
@@ -58,58 +58,39 @@ def force(r_,planet_,ro_,vo_):
     if planet_ == 'jupiter':
         return force_js(r_) - force_ej(r_,ro_)
 
-    
+
 def dr_dt(t_,r_,v_,planet_,ro_,vo_):
     return v_
- 
-    
+
+
 def dv_dt(t_,r_,v_,planet_,ro_,vo_):
     F = force(r_,planet_,ro_,vo_)
-    if planet_ == 'earth':
-        y = F/Me
-    if planet_ == 'jupiter':
-        y = F/Mj
-    return y 
-
-# Differential equation solvers
-# ===================================================================
-def EulerSolver(t_,r_,v_,h_):
-    r1 = r_ + h_*dr_dt(t_,r_,v_)
-    v1 = v_ + h_*dv_dt(t_,r_,v_)
-    z = [r1, v1]
-    return z
-
-def EulerCromerSolver(t_,r_,v_,h_):
-    r_ += h_*dr_dt(t_,r_,v_)
-    v_ += h_*dv_dt(t_,r_,v_)
-    z = [r_, v_]
-    return z
+    y = F/planet_.m
+    return y
 
 def RK4Solver(t_,r_,v_,h_,planet,ro,vo):
-    k11 = dr_dt(t_,r_,v_,planet,ro,vo) 
+    k11 = dr_dt(t_,r_,v_,planet,ro,vo)
     k21 = dv_dt(t_,r_,v_,planet,ro,vo)
-    
+
     k12 = dr_dt(t_ + 0.5*h_,r_ + 0.5*h_*k11,v_ + 0.5*h_*k21,planet,ro,vo)
     k22 = dv_dt(t_ + 0.5*h_,r_ + 0.5*h_*k11,v_ + 0.5*h_*k21,planet,ro,vo)
-    
+
     k13 = dr_dt(t_ + 0.5*h_,r_ + 0.5*h_*k12,v_ + 0.5*h_*k22,planet,ro,vo)
     k23 = dv_dt(t_ + 0.5*h_,r_ + 0.5*h_*k12,v_ + 0.5*h_*k22,planet,ro,vo)
-    
+
     k14 = dr_dt(t_ + h_,r_ + h_*k13,v_ + h_*k23,planet,ro,vo)
     k24 = dv_dt(t_ + h_,r_ + h_*k13,v_ + h_*k23,planet,ro,vo)
-    
+
     y0 = r_ + h_ * (k11 + 2.*k12 + 2.*k13 + k14) / 6.
     y1 = v_ + h_ * (k21 + 2.*k22 + 2.*k23 + k24) / 6.
-    
+
     z = [y0, y1]
     return z
-
-# =====================================================================
 
 
 def KineticEnergy(v_):
     vn = np.linalg.norm(v_)
-    return 0.5*Me*vn**2
+    return 0.5*EARTH_MASS*vn**2
 
 def PotentialEnergy(r_):
     fmag = np.linalg.norm(force_es(r_))
@@ -123,81 +104,38 @@ def AngMomentum(r_,v_):
     v_ = v_/vn
     rdotv = r_[0]*v_[0]+r_[1]*v_[1]
     theta = math.acos(rdotv)
-    return Me*rn*vn*np.sin(theta)
+    return EARTH_MASS*rn*vn*np.sin(theta)
 
-def AreaCalc(r1,r2):
-    r1n = np.linalg.norm(r1)
-    r2n = np.linalg.norm(r2)
-    r1 = r1 + 1e-20
-    r2 = r2 + 1e-20
-    theta1 = math.atan(abs(r1[1]/r1[0]))
-    theta2 = math.atan(abs(r2[1]/r2[0]))
-    rn = 0.5*(r1n+r2n)
-    del_theta = np.abs(theta1 - theta2)
-    return 0.5*del_theta*rn**2
+_1 = Body(m=6e24) # Earth
+_2 = Body(m=1.9e27) # Jupiter
+_3 = Body(m=2e30) # Sun
+G = 6.673e-11
+AU_KM = 1.496e11
+EARTH_MASS = 6e24
+YEAR_SEC = 365*24*60*60.0
+FF = (G*EARTH_MASS**2)/AU_KM**2          # Unit force
+EE = FF*AU_KM                    # Unit energy
+GG = (EARTH_MASS*G*YEAR_SEC**2)/(AU_KM**3)
+initial_time = 0
+final_time = 120
+points = 100*final_time
+t = np.linspace(initial_time,final_time,points)
+time_step = t[2]-t[1] # time step (uniform)
 
-def mplot(fign,x,y,xl,yl,clr,lbl):
-    py.figure(fign)
-    py.xlabel(xl)    
-    py.ylabel(yl)
-    return py.plot(x,y,clr, linewidth =1.0,label = lbl)
-
-
-Me = 6e24                     # Mass of Earth in kg
-Ms = 2e30                     # Mass of Sun in kg                       
-Mj = 1.9e27                   # Mass of Jupiter
-
-G = 6.673e-11                 # Gravitational Constant
-
-RR = 1.496e11                 # Normalizing distance in km (= 1 AU)
-MM = 6e24                     # Normalizing mass
-TT = 365*24*60*60.0           # Normalizing time (1 year)
-
-FF = (G*MM**2)/RR**2          # Unit force
-EE = FF*RR                    # Unit energy
-
-GG = (MM*G*TT**2)/(RR**3)
-
-Me = Me/MM                    # Normalized mass of Earth
-Ms = Ms/MM                    # Normalized mass of Sun  
-Mj = 500*Mj/MM                # Normalized mass of Jupiter/Super Jupiter
-
-
-ti = 0                        # initial time = 0
-tf = 120                      # final time = 120 years
-
- 
-
-
-N = 100*tf                   # 100 points per year
-t = np.linspace(ti,tf,N)     # time array from ti to tf with N points 
-
-h = t[2]-t[1]                # time step (uniform)
-
-
-
-
-# Initialization
-
-KE = np.zeros(N)            # Kinetic energy
-PE = np.zeros(N)            # Potential energy
-AM = np.zeros(N)            # Angular momentum
-AreaVal = np.zeros(N)
-
-r = np.zeros([N,2])         # position vector of Earth
-v = np.zeros([N,2])         # velocity vector of Earth
-rj = np.zeros([N,2])        # position vector of Jupiter
-vj = np.zeros([N,2])        # velocity vector of Jupiter
-
-ri = [1496e8/RR,0]          # initial position of earth
+AreaVal = np.zeros(points)
+r = np.zeros([points,2])         # position vector of Earth
+v = np.zeros([points,2])         # velocity vector of Earth
+rj = np.zeros([points,2])        # position vector of Jupiter
+vj = np.zeros([points,2])        # velocity vector of Jupiter
+ri = [1496e8/AU_KM,0]          # initial position of earth
 rji = [5.2,0]               # initial position of Jupiter
 
 
 
 
-vv = np.sqrt(Ms*GG/ri[0])         # Magnitude of Earth's initial velocity 
+vv = np.sqrt(Ms*GG/ri[0])         # Magnitude of Earth's initial velocity
 
-vvj = 13.06e3 * TT/RR             # Magnitude of Jupiter's initial velocity 
+vvj = 13.06e3 * YEAR_SEC/AU_KM             # Magnitude of Jupiter's initial velocity
 
 vi = [0, vv*1.0]                  # Initial velocity vector for Earth.Taken to be along y direction as ri is on x-axis.
 vji = [0, vvj*1.0]                # Initial velocity vector for Jupiter
@@ -205,26 +143,18 @@ vji = [0, vvj*1.0]                # Initial velocity vector for Jupiter
 
 
 
-t[0] = ti
+t[0] = initial_time
 r[0,:] = ri
 v[0,:] = vi
 rj[0,:] = rji
 vj[0,:] = vji
 
-KE[0] = KineticEnergy(v[0,:])
-PE[0] = PotentialEnergy(r[0,:])
-AM[0] = AngMomentum(r[0,:],v[0,:])
-AreaVal[0] = 0
 
- 
-for i in range(0,N-1):
-    [r[i+1,:],v[i+1,:]]=RK4Solver(t[i],r[i,:],v[i,:],h,'earth',rj[i,:],vj[i,:])
-    [rj[i+1,:],vj[i+1,:]]=RK4Solver(t[i],rj[i,:],vj[i,:],h,'jupiter',r[i,:],v[i,:])
-        
-    KE[i+1] = KineticEnergy(v[i+1,:])
-    PE[i+1] = PotentialEnergy(r[i+1,:])
-    AM[i+1] = AngMomentum(r[i+1,:],v[i+1,:])
-    AreaVal[i+1] = AreaVal[i] + AreaCalc(r[i,:],r[i+1,:])
+
+for i in range(0,points-1):
+    r[i+1,:],v[i+1,:]=RK4Solver(t[i],r[i,:],v[i,:],time_step,'earth',rj[i,:],vj[i,:])
+    rj[i+1,:],vj[i+1,:]=RK4Solver(t[i],rj[i,:],vj[i,:],time_step,'jupiter',r[i,:],v[i,:])
+
 
 
 def animate(i_):
